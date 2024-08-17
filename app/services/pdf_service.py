6b980b7a4f269611ce49ec73eb2d203b8225c2f3
Fascinat0r <fascinator2904@gmail.com>
2024-08-18 02:50:15 +0300

    Cleanup service layer

--- app/services/pdf_service.py
+++ app/services/pdf_service.py
@@ -1,9 +1,8 @@
-from datetime import datetime
-
 from app.interfaces.pdf_service_interface import PDFServiceInterface
-from app.models.pdf_models import LiftCompanyReport, LiftReport
+from app.models.pdf_models import LiftCompanyReport
 from app.processors.pdf.pdf_processor import PDFProcessor
 from app.repositories.pdf_repository import PDFRepository
+from app.services import utils
 from app.services.config_loader import ConfigLoader
 
 
@@ -15,6 +14,10 @@ class PDFService(PDFServiceInterface):
     def process_lift_pdf(self, pdf_path: str, output_path=None) -> list[LiftCompanyReport]:
         """
         Обрабатывает PDF-документ о простое лифтов и возвращает массив моделей данных.
+
+        :param pdf_path: Путь к PDF-файлу
+        :param output_path: Путь для сохранения размеченного PDF-файла (необязательный)
+        :return: Массив моделей LiftCompanyReport
         """
         config_path = "core/configs/pdf_structures/lift_report_test.yml"
         try:
@@ -33,7 +36,7 @@ class PDFService(PDFServiceInterface):
             print("PDF обработан")
 
             # Преобразование в модели данных
-            lift_company_reports = self._convert_to_models(extracted_data)
+            lift_company_reports = utils.convert_to_models(extracted_data)
             return lift_company_reports
 
         except Exception as e:
@@ -43,6 +46,8 @@ class PDFService(PDFServiceInterface):
     def validate_pdf(self, pdf_path: str) -> None:
         """
         Проверяет наличие и корректность PDF-документа.
+
+        :param pdf_path: Путь к PDF-файлу
         """
         try:
             self.repository.load_pdf(pdf_path)
@@ -61,49 +66,3 @@ class PDFService(PDFServiceInterface):
         except Exception as e:
             print(f"Ошибка валидации PDF: {e}")
             raise e
-
-    def _convert_to_models(self, extracted_data) -> list[LiftCompanyReport]:
-        """
-        Преобразует извлеченные данные в модели LiftCompanyReport и LiftReport с валидацией.
-        """
-        lift_company_reports = []
-
-        for block in extracted_data['stoppages_data']:
-            company_name = block['block']['company_name']
-            company_report = LiftCompanyReport(company_name)
-
-            for row in block['rows']:
-                start_time = self._convert_to_rfc3339(row.get('start_time', ''))
-                end_time = self._convert_to_rfc3339(row.get('end_time', '')) if row.get('end_time') else ''
-                downtime_hours = row.get('downtime_hours', '')
-                factory_number = row.get('factory_number', '')
-                reg_number = row.get('serial_number', '')
-
-                # Валидация обязательных полей
-                if not all([start_time, downtime_hours, factory_number, reg_number]):
-                    raise ValueError(f"Обязательные поля отсутствуют или пусты: {row}")
-
-                lift_report = LiftReport(
-                    start_time=start_time,
-                    end_time=end_time,
-                    downtime_hours=int(downtime_hours),
-                    factory_number=factory_number,
-                    reg_number=reg_number
-                )
-                company_report.reports.append(lift_report)
-
-            lift_company_reports.append(company_report)
-
-        return lift_company_reports
-
-    @staticmethod
-    def _convert_to_rfc3339(datetime_str: str) -> str:
-        """
-        Преобразует строку с датой и временем в формат RFC 3339.
-        Ожидаемый формат строки: 'дд.мм.гггг чч:мм'
-        """
-        try:
-            dt = datetime.strptime(datetime_str, "%d.%m.%Y %H:%M")
-            return dt.isoformat()
-        except ValueError as e:
-            raise ValueError(f"Ошибка преобразования времени '{datetime_str}': {e}")
---
+++ app/services/utils.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+from app.models.pdf_models import LiftCompanyReport, LiftReport
+
+
+def convert_to_models(self, extracted_data) -> list[LiftCompanyReport]:
+    """
+    Преобразует извлеченные данные в модели LiftCompanyReport и LiftReport с валидацией.
+    """
+    lift_company_reports = []
+
+    for block in extracted_data['stoppages_data']:
+        company_name = block['block']['company_name']
+        company_report = LiftCompanyReport(company_name)
+
+        for row in block['rows']:
+            start_time = self.convert_to_rfc3339(row.get('start_time', ''))
+            end_time = self.convert_to_rfc3339(row.get('end_time', '')) if row.get('end_time') else ''
+            downtime_hours = row.get('downtime_hours', '')
+            factory_number = row.get('factory_number', '')
+            reg_number = row.get('serial_number', '')
+
+            # Валидация обязательных полей
+            if not all([start_time, downtime_hours, factory_number, reg_number]):
+                raise ValueError(f"Обязательные поля отсутствуют или пусты: {row}")
+
+            lift_report = LiftReport(
+                start_time=start_time,
+                end_time=end_time,
+                downtime_hours=int(downtime_hours),
+                factory_number=factory_number,
+                reg_number=reg_number
+            )
+            company_report.reports.append(lift_report)
+
+        lift_company_reports.append(company_report)
+
+    return lift_company_reports
+
+
+def convert_to_rfc3339(datetime_str: str) -> str:
+    """
+    Преобразует строку с датой и временем в формат RFC 3339.
+    Ожидаемый формат строки: 'дд.мм.гггг чч:мм'
+    """
+    try:
+        dt = datetime.strptime(datetime_str, "%d.%m.%Y %H:%M")
+        return dt.isoformat()
+    except ValueError as e:
+        raise ValueError(f"Ошибка преобразования времени '{datetime_str}': {e}")
